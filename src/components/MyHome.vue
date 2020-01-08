c80b158542a43a4d32f001ff163621be3c993880
Admin <1318399699@qq.com>
2020-01-09 00:22:13 +0800

    提交了啥

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,6 +1,16 @@
 <template>
-  <el-calendar v-model="value">
-  </el-calendar>
+  <div>
+    TODO : <br/>
+    1. 菜单-面包屑 关联显示 √<br/>
+    2. 菜单缩小，主界面同步变大<br/>
+    3. 菜单图标跳转首页 √<br/>
+    4. 个人图标的下选功能 修改密码、退出登录、编辑个人资料、已读未读消息预留位置等<br/>
+    5. 菜单树完善：拖拽节点（已完成）、编辑节点、新增节点、删除节点<br/>
+    6. 权限列表crud<br/>
+    7. 角色列表c（关联权限）ru（修改所关联的权限）d（逻辑删除）<br/>
+    8. 用户列表crud及用户指定角色 一用户可设置多角色<br/>
+    9. 登录页面<br/>
+  </div>
 </template>
 
 <script>
@@ -8,7 +18,6 @@
         name: "MyHome",
         data() {
             return {
-                value: new Date()
             }
         }
     }
